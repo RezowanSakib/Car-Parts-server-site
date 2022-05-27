43ed9beaea8706ccc5533fec1d88c2e1b1c445ff
Sakib Md Rezowan <sakibmdrezowan@gmail.com>
2022-05-27 14:48:02 +0600

    single product added

--- index.js
+++ index.js
@@ -1,26 +1,64 @@
+// const express = require("express");
+// const cors = require("cors");
+// require("dotenv").config();
+// const { MongoClient, ServerApiVersion } = require("mongodb");
+// const app = express();
+// const port = process.env.PORT || 5000;
+// app.use(cors());
+// app.use(express.json);
+// //sakib
+// //ZcrZTstbXWI56EMC
+
+// const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@carpartcluster.pau1djg.mongodb.net/?retryWrites=true&w=majority`;
+// console.log(uri);
+// const client = new MongoClient(uri, {
+//   useNewUrlParser: true,
+//   useUnifiedTopology: true,
+//   serverApi: ServerApiVersion.v1,
+// });
+
+// async function run() {
+//   try {
+//     await client.connect();
+//     const productCollection = client.db("CarPars").collection("Parts");
+
+//     //get all data
+//     app.get("/product", async (req, res) => {
+//       const query = {};
+//       const cursor = productCollection.find(query);
+//       const products = await cursor.toArray();
+//       res.send(products);
+//     });
+//   } finally {
+//   }
+// }
+
+// app.get("/", (req, res) => {
+//   res.send("Hello World!");
+// });
+
+// app.listen(port, () => {
+//   console.log(`Example app listening on port ${port}`);
+// });
 const express = require("express");
 const cors = require("cors");
+const { MongoClient, ServerApiVersion , ObjectId } = require("mongodb");
 require("dotenv").config();
- const { MongoClient, ServerApiVersion } = require("mongodb");
 const app = express();
 const port = process.env.PORT || 5000;
 app.use(cors());
-app.use(express.json);
-//sakib
-//ZcrZTstbXWI56EMC
-
+app.use(express.json());
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@carpartcluster.pau1djg.mongodb.net/?retryWrites=true&w=majority`;
-console.log(uri);
 const client = new MongoClient(uri, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   serverApi: ServerApiVersion.v1,
 });
-
 async function run() {
   try {
     await client.connect();
     const productCollection = client.db("CarPars").collection("Parts");
+
     //get all data
     app.get("/product", async (req, res) => {
       const query = {};
@@ -28,12 +66,19 @@ async function run() {
       const products = await cursor.toArray();
       res.send(products);
     });
+    app.get("/product/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const product = await productCollection.findOne(query);
+      res.send(product);
+    });
   } finally {
   }
 }
+run().catch(console.dir);
 
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.send("Hello World! ");
 });
 
 app.listen(port, () => {
